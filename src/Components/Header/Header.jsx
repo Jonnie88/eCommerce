33b7bbdab45f04cf43b21330de128aef9b63a67c
Jonnie88 <32672511+Jonnie88@users.noreply.github.com>
2023-04-21 11:17:32 +0200

    navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Header.css";
-
+import { NavLink } from "react-router-dom";
+import { BsPerson } from "react-icons/bs";
 function Header() {
-  return <div className="header">Header</div>;
+  return (
+    <>
+      <div className="alert">Free shipping</div>
+      <nav className="header">
+        <div className="container">
+          <NavLink className="logo" to="/">
+            Matsmart
+          </NavLink>
+          <input type="text" placeholder="Search.." />
+          <div className="btn-container">
+            <button className="btn btn-secondary">
+              <BsPerson size={24} /> Login
+            </button>
+            <button className="btn btn-secondary">Checkout</button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
